8063e7b7b5c86b4996feb020dc1f25252641257e
Kiritek <ozdzinskipiotr@gmail.com>
2021-11-09 20:19:13 +0100

    added 20 js modifiers

--- lab1/js/scriptsform.js
+++ lab1/js/scriptsform.js
@@ -5,10 +5,24 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+"use strict";
+document.body.style.backgroundColor = "#FFF59E";
+document.body.style.fontFamily = "Georgia";
+document.body.style.fontSize = "110%";
+document.getElementById("formimage").style.filter="grayscale(100%)";
+let formbutton = document.getElementById("formbutton");
+formbutton.onclick = CreateForm;
+formbutton.className  = "btn btn-secondary";
 
-document.getElementById("formbutton").onclick = createForm;
+let formtile = document.getElementById("formintro");
 
-function createForm(){
+let formElements = document.getElementsByClassName("formcentered");
+for(let i = 0; i< formElements.length;i++){
+    formElements[i].style.padding = "0.95%";
+}
+
+
+function CreateForm(){
     let nickname = document.getElementById("nickname").value
     let email = document.getElementById("email").value
     let pick1 = document.getElementById("pick1").value
@@ -19,7 +33,7 @@ function createForm(){
 
     document.getElementById("formsummaryid").style.visibility = "visible" ;
     document.getElementById("formbutton").style.visibility = "hidden";
-
+    document.getElementById("campaignform").style.display = "none";
     document.getElementById("nicksummary").appendChild(document.createTextNode(": "+nickname))
     document.getElementById("emailsummary").appendChild(document.createTextNode(": "+email))
     document.getElementById("pick1summary").appendChild(document.createTextNode(": "+pick1))
@@ -27,8 +41,13 @@ function createForm(){
     document.getElementById("pick3summary").appendChild(document.createTextNode(": "+pick3))
     document.getElementById("pick4summary").appendChild(document.createTextNode(": "+pick4))
     document.getElementById("pick5summary").appendChild(document.createTextNode(": "+pick5))
-
-
-
+    
+    let formsummary=document.getElementById("formsummaryid");
+    formsummary.style.border="3px solid black";
+    formsummary.style.width="75%";
+    formsummary.style.color="green";
+    formsummary.style.opacity="70%";
+    formsummary.style.textDecoration="underline";
+    formsummary.style.textShadow="1px 1px blue";
 
 }   
---
+++ lab1/js/scriptslist.js
@@ -0,0 +1,9 @@
+let listtitle = document.getElementById("listtitle");
+listtitle.style.fontSize="5rem";
+listtitle.style.textAlign="center";
+listtitle.style.fontWeight="bold";
+listtitle.style.letterSpacing="3px";
+let signature = document.getElementById("signature")
+signature.style.fontWeight = "lighter";
+signature.style.textAlign="right";
+signature.style.marginRight="30px";
