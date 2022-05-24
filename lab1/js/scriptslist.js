e690a8e1eef0893aa7ba2e0d2f315fe888c2fc84
Piotr Ożdziński <84562925+Kiritek@users.noreply.github.com>
2022-05-24 17:19:01 +0200

    Delete lab1 directory

--- lab1/js/scriptslist.js
+++
@@ -1,9 +0,0 @@
-let listtitle = document.getElementById("listtitle");
-listtitle.style.fontSize="5rem";
-listtitle.style.textAlign="center";
-listtitle.style.fontWeight="bold";
-listtitle.style.letterSpacing="3px";
-let signature = document.getElementById("signature")
-signature.style.fontWeight = "lighter";
-signature.style.textAlign="right";
-signature.style.marginRight="30px";
