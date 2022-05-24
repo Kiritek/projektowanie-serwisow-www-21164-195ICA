0429ef3e389d07bd06538d283291d10b470dede3
Piotr Ożdziński <84562925+Kiritek@users.noreply.github.com>
2022-05-24 17:22:21 +0200

    Delete lab8 directory

--- lab8/src/components/Image.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-//Zwrocenie htmlu obrazka z źródłem i podpisem z destrukturyzowanych propsów
-const Image = ({ url, title }) => (
-  <li>
-    <img src={url} alt={title} />
-  </li>
-);
-
-export default Image;
